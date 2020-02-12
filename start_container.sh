44f47cd27ce495bdb6a5b5fc3b986bb0098f4ec9
Maximilian Swiatlowski <mswiatlowski@triumf.ca>
2020-02-12 09:20:20 -0800

    update to 1p14 container

--- start_container.sh
+++ start_container.sh
@@ -1,2 +1,2 @@
 dig +short myip.opendns.com @resolver1.opendns.com > ip.txt
-singularity exec --nv -B /fast_scratch -B /data /fast_scratch/triumfmlutils/containers/baseml_tf_v0.1.9.sif  /bin/bash
+singularity exec --nv -B /fast_scratch -B /data /fast_scratch/triumfmlutils/containers/baseml_tf_v0.1.14.sif  /bin/bash
