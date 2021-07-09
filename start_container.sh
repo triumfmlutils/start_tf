c186d3ee0ecca0ccb3575aece5ab5dda15495d8e
Maximilian Swiatlowski <mswiatlowski@triumf.ca>
2021-07-09 10:59:26 -0700

    update to 34

--- start_container.sh
+++ start_container.sh
@@ -1,2 +1,2 @@
 dig +short myip.opendns.com @resolver1.opendns.com > ip.txt
-singularity exec --nv -B /fast_scratch -B /data /fast_scratch/triumfmlutils/containers/baseml_tf_v0.1.33.sif  /bin/bash
+singularity exec --nv -B /fast_scratch -B /data /fast_scratch/triumfmlutils/containers/baseml_tf_v0.1.34.sif  /bin/bash
